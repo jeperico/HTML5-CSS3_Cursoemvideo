6cf7445ffe2103bc768dcd58012755cdbddacd89
jeperico <jeperico@terra.com.br>
2023-09-09 12:56:49 -0300

    ex060

--- Modulo_04/Exercícios/ex060/Menu.js
+++ Modulo_04/Exercícios/ex060/Menu.js
@@ -1,7 +1,18 @@
+
 function Menu(){
     const Menu = document.querySelector('#Menu');
+    
+    if(Menu.style.display == 'block'){
+        Menu.style.display= 'none';
+    }else{
+        Menu.style.display= 'block';
+    }
+}
 
-    if(Menu.style.display == 'none'){
+function Size(){
+    const Menu = document.querySelector('#Menu');
+    
+    if(window.innerWidth >= 768){
         Menu.style.display= 'block';
     }else{
         Menu.style.display= 'none';
